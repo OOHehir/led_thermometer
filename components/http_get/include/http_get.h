90c7f50bf304371f7f1c8b56477e90ae879250da
OOHehir <40183643+OOHehir@users.noreply.github.com>
2024-12-14 16:37:02 +0000

    Create component setup

---
+++ components/http_get/include/http_get.h
@@ -0,0 +1,18 @@
+/**
+ * @file http_get.h
+ * @author The Authors
+ * @brief
+ * @version 0.1
+ * @date 2024-12-14
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#ifndef __HTTP_GET_H
+#define __HTTP_GET_H
+
+void http_get_task(void *pvParameters);
+
+
+#endif // __HTTP_GET_H
