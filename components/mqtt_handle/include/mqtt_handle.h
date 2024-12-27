bfb7cf369de8f251e121d8240fdc2ac02a140965
OOHehir <40183643+OOHehir@users.noreply.github.com>
2024-12-27 10:00:51 +0000

    Add MQTT

---
+++ components/mqtt_handle/include/mqtt_handle.h
@@ -0,0 +1,21 @@
+/**
+ * @file mqtt.h
+ * @author The Authors
+ * @brief
+ * @version 0.1
+ * @date 2024-12-26
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#ifndef MQTT_H_
+#define MQTT_H_
+
+void mqtt5_start(void);
+
+
+
+
+
+#endif  /* MQTT_H_ */
